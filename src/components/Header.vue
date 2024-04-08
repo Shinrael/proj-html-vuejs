c74af9d956deafa33f1cc7f04d1ac08f683b1302
Shinrael <mircoelena89@gmail.com>
2024-04-08 14:11:03 +0100

    [add] header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,6 +5,24 @@ import { store } from '../data/store'
     data(){
       return{
         store,
+        socialIcon : [
+    {
+      icon: 'fa-brands fa-facebook-f',
+      title: 'Facebook'
+    },
+    {
+      icon: 'fa-brands fa-twitter',
+      title: 'Twitter'
+    },
+    {
+      icon:'fa-brands fa-instagram',
+      title: 'Instagram'
+    },
+    {
+      icon: 'fa-brands fa-youtube',
+      title: 'Youtube'
+    }
+  ],
       }
     }
     
@@ -13,13 +31,46 @@ import { store } from '../data/store'
 
 
 <template>
-  <div>
-    Header
-  </div>
+  <header class="d-flex justify-content-center ">
+    <div class="container-header d-flex">
+      <div class="h-left d-flex text-white align-items-center ">
+        <i class="fa-solid fa-phone-flip me-3"></i>
+        <p>Call us for a Free Quote: 1.800.555.6789</p>
+      </div>
+      <div class="h-right d-flex text-white align-items-center justify-content-end">
+        
+          <ul class="list-inline ">
+            <li
+              v-for="icon in socialIcon"
+              :key="icon.title"
+              class="list-unstyled list-inline-item ms-2">
+              <i :class="icon.icon"></i>
+            </li>
+          </ul>
+        
+      </div>
+    </div>    
+  </header>
 </template>
 
 
 
 <style lang="scss" scoped>
-
+  header{
+    height: 90px;
+    background-color: #6bbb14;
+    .container-header{
+      width: 60%;
+      .h-left{
+        width: 50%;
+        i{
+          padding-bottom: 16px;
+        }
+      }  
+      .h-right{
+        width: 50%;
+      }  
+      
+    }
+  }
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -1,5 +1,22 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-  
+  socialIcon : [
+    {
+      icon: 'fa-brands fa-facebook-f',
+      title: 'Facebook'
+    },
+    {
+      icon: 'fa-brands fa-twitter',
+      title: 'Twitter'
+    },
+    {
+      icon:'fa-brands fa-instagram',
+      title: 'Instagram'
+    },
+    {
+      icon: 'fa-brands fa-youtube',
+      title: 'Youtube'
+    }
+  ],
 })
